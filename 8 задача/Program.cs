d26c01ae52f6852e435e215e6c2f9371cf19df36
Nik <neket282007@gmail.com>
2022-10-04 21:54:12 +0300

    8 задача

---
+++ 8 задача/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число n");
+int n = int.Parse(Console.ReadLine()!);
+int i = 1;
+Console.WriteLine("все чётные числа в диапозне от 1 до n");
+while (i<=n)
+{
+    if (i%2 ==0){
+        Console.WriteLine($"{i}");
+    }
+    i++;
+}
