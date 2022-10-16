492fd84f1331d6a477c8ad50ee15e007adf5816d
Nik <neket282007@gmail.com>
2022-10-16 22:51:25 +0300

    27  задача

---
+++ 27 задача/Program.cs
@@ -0,0 +1,15 @@
+﻿ Console.WriteLine("Введите число сумму цифр которно хотите найти: ");
+  String number = Console.ReadLine()!;
+ Console.WriteLine($"Результат: {DoSum(number)}");
+
+  int DoSum (String number)
+  {
+      int result = 0;
+      int number2 = Convert.ToInt32(number);
+      for (int i = 0; i < number.Length; i++) 
+      {
+          result += number2%10;
+          number2 /= 10;
+      }
+      return result;
+  }
