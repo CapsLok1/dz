bcead0938173bed55e74303f2f7d2771fe07b9d2
Nik <neket282007@gmail.com>
2022-10-16 22:54:35 +0300

    29  задача

---
+++ 29 задача/Program.cs
@@ -0,0 +1,11 @@
+﻿ Console.WriteLine($"Массив состоящий из 8 элементов с рандомными числами от 0 до 1000: [{String.Join(", " , DoArr())}]");
+// создаем метод, который может вывести массив
+ int[] DoArr()
+ {
+     int[] result = new int[8];
+     for (int i=0; i<8;i++)
+     {
+         result[i] = new Random().Next(1001);
+     }
+     return result;
+ }
