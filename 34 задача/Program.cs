047f0c88cb39eb095efab0efb7d5b7fc370e9219
Nik <neket282007@gmail.com>
2022-10-18 22:33:18 +0300

    переделанная 41  задача

--- 34 задача/Program.cs
+++ 34 задача/Program.cs
@@ -2,24 +2,25 @@
 
 // [345, 897, 568, 234] -> 2
 
- int[] arr = DoArr();
- Console.WriteLine($"Массив с рандомными трёхзначными числами: [{String.Join(", ", arr)}], колличество четных чисел: {CheckEvenNumbers(arr)}");
- int[] DoArr()
- {
-     int[] result = new int[8];
-     for (int i=0; i<8;i++)
-     {
-         result[i] = new Random().Next(1001);
-     }
-     return result;
- }
+  int[] arr = DoArr();
+  Console.WriteLine($"Массив с рандомными трёхзначными числами: [{String.Join(", ", arr)}], колличество четных чисел: {CheckEvenNumbers(arr)}");
+  int[] DoArr()
+  {
+      int[] result = new int[8];
+      for (int i=0; i<8;i++)
+      {
+          result[i] = new Random().Next(1001);
+      }
+      return result;
+  }
  
-  int CheckEvenNumbers(int[] arr){
-     int result = 0;
-     for (int i=0; i<10;i++){
-         result = result%2;
-         result++;
-     }
-     return result;
+   int CheckEvenNumbers(int[] arr){
+      int result = 0;
+      for (int i=0; i<arr.Length;i++){
+          if (arr[i] % 2 == 0){
+          result++;
+          }
+      }
+      return result;
 
-  } 
+   } 
