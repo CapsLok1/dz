ba3332c0ecfb1632309b33de8ecb7a4e96a52622
Nik <neket282007@gmail.com>
2022-10-18 22:22:24 +0300

    41  задача

---
+++ 41 задача/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите размерность");
+int a = int.Parse(Console.ReadLine()!);
+int[] array = new int[a];
+GetRandomArr(array);
+Console.WriteLine($"[{String.Join(" ", array)}]");
+Console.WriteLine($"всего натуралыных чисел в данном массиве - {GetNaturalsNumberas(array)}");
+void  GetRandomArr(int[] quan)
+{
+    for (int i=0; i<quan.Length;i++)
+    {
+        quan[i] = new Random().Next(-100, 101);
+    }
+}
+
+int GetNaturalsNumberas(int[] array){
+  int sum = 0;
+  for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        sum++;
+    }
+}
+    return sum;
+}
