a9f4e1b909ea8a4a2196e193284fd5143158c4ac
Nik <neket282007@gmail.com>
2022-10-18 21:23:13 +0300

    38  задача

---
+++ 38 задача/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите размерность");
+int a = int.Parse(Console.ReadLine()!);
+int[] array = new int[a];
+GetRandomArr(array);
+Console.WriteLine($"[{String.Join(" ", array)}]");
+Console.WriteLine($"Разница = {DoDifference(array)}");
+void  GetRandomArr(int[] quan)
+{
+    for (int i=0; i<quan.Length;i++)
+    {
+        quan[i] = new Random().Next(-100, 101);
+    }
+}
+
+int  DoDifference(int[] array){
+    int min = array[0];
+    int max = array[0];
+    foreach(var item in array){
+        if (min>item) min = item;
+        if (max<item) max = item;
+
+    }
+    return max-min;
+}
+
